19be3021b5a26bf0f06964756b8ad780468907a9
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-07-03 11:25:56 +0200

    Added timer and new lower third

--- vueFrontend/src/components/allComponents.ts
+++ vueFrontend/src/components/allComponents.ts
@@ -1,9 +1,14 @@
 import LowerThird from "./LowerThird.vue";
+import LowerThirdFull from "./LowerThirdFull.vue";
 import Subtitle from "./Subtitle.vue";
+import { TextComponent } from "./TextComponent";
+import Timer from "./Timer.vue";
 
 export const allComponents = {
     LowerThird,
+    LowerThirdFull,
     Subtitle,
+    Timer,
 };
 
 export const allComponentsList: { name: string; component: any }[] = [
@@ -12,7 +17,15 @@ export const allComponentsList: { name: string; component: any }[] = [
         component: LowerThird,
     },
     {
+        name: "LowerThirdFull",
+        component: LowerThirdFull,
+    },
+    {
         name: "Subtitle",
         component: Subtitle,
+    },
+    {
+        name: "Timer",
+        component: Timer,
     },
 ];
