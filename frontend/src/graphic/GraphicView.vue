95fa0e0f9df6bac57949ea61092b9bab3529c20b
ModellbahnFreak <freak.mdellbahn@gmail.com>
2024-06-14 02:25:17 +0200

    Working lower third

--- frontend/src/graphic/GraphicView.vue
+++ frontend/src/graphic/GraphicView.vue
@@ -30,7 +30,7 @@ html {
     left: 0;
     top: 0;
     width: 100vw;
-    height: 100vw;
+    height: 100vh;
     margin: 0 0 0 0;
     padding: 0 0 0 0;
 }
--- plugins/base_titles/frontend/graphic/LowerThird.vue
+++ plugins/base_titles/frontend/graphic/LowerThird.vue
@@ -1,16 +1,44 @@
 <script setup lang="ts">
 import baseTitles from "../index";
 import { LowerThirdDataKey } from "../index";
-import { computed } from 'vue';
+import { computed, watch, Ref, ref } from 'vue';
+
+const LOWER_THIRD_WIDTH = "84vw";
 
 const api = await baseTitles.api
 
 const lowerThird = await api.getDataKey<LowerThirdDataKey>("lowerThird");
 
 const isLogoActive = computed(() => {
-    return lowerThird.value?.logoUrl?.value && logoUrl.value.length > 0
+    return lowerThird?.value?.logoUrl && lowerThird.value.logoUrl.length > 0
 });
 
+const outerWidth = ref("0");
+const innerOpacity = ref("0");
+
+
+watch(lowerThird as unknown as Ref<unknown>, (newState: LowerThirdDataKey, oldState: LowerThirdDataKey) => {
+    console.log("Watch", newState, oldState);
+    if (newState?.isActive) {
+        outerWidth.value = LOWER_THIRD_WIDTH;
+        setTimeout(() => {
+            if (lowerThird?.value?.isActive) {
+                innerOpacity.value = "1";
+            } else {
+                innerOpacity.value = "0";
+            }
+        }, 200);
+    } else {
+        innerOpacity.value = "0";
+        setTimeout(() => {
+            if (!lowerThird?.value?.isActive) {
+                outerWidth.value = "0";
+            } else {
+                outerWidth.value = LOWER_THIRD_WIDTH;
+            }
+        }, 200);
+    }
+});
 </script>
 
 <style>
@@ -53,10 +81,11 @@ const isLogoActive = computed(() => {
 </style>
 
 <template>
-    <div class="lowerThirdOuterContainer" ref="lowerThirdOuterContainer">
-        <v-row class="lowerThirdContainer" ref="lowerThirdContainer" :style="{
-            height: !!lowerThird?.subtitle ? '12.5vh' : '9vh',
-        }">
+    <div class="lowerThirdOuterContainer" :style="{ width: outerWidth }">
+        <v-row class="lowerThirdContainer" :style="{
+        height: !!lowerThird?.subtitle ? '12.5vh' : '9vh',
+        opacity: innerOpacity
+    }">
             <v-col style="flex-grow: 0; flex-shrink:0;height:100%;" class="pa-0 mr-3 d-flex align-center">
                 <img :src="lowerThird?.logoUrl" style="height: 8.5vh;"
                     :style="{ 'display': isLogoActive ? undefined : 'none' }" />
