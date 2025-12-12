5c24e45cc7f1ec2161c78f976691643ac741b826
ModellbahnFreak <freak.mdellbahn@gmail.com>
2025-12-12 22:40:00 +0100

    added connection indicator to viewer

--- frontend/src/views/graphic/GraphicOptions.vue
+++ frontend/src/views/graphic/GraphicOptions.vue
@@ -3,13 +3,20 @@ import { useComponentStore } from '@/code/pluginManagement/componentStore';
 import { usePluginStore } from '@/code/pluginManagement/pluginStore';
 import type { ComponentMetadata } from '@videotextgenerator/api';
 import SharedOptions from "@/views/editor/SharedOptions.vue";
-import { ref } from 'vue';
+import { inject, ref } from 'vue';
+import type { SocketsManager } from '@/code/backend/SocketsManager';
 
 const componentStore = useComponentStore();
 const pluginStore = usePluginStore();
 
 const props = defineProps(["clientConfig"]);
 const emit = defineEmits(["configChanged"]);
+
+const isConnected = ref(true);
+const socketsManager = inject("socketsManager") as SocketsManager;
+setInterval(() => {
+    isConnected.value = socketsManager.numOpenSockets > 0;
+}, 1000);
 
 const isDialogOpened = ref(false);
 
@@ -30,6 +37,7 @@ function displayName(componentData: ComponentMetadata): string {
         <template v-slot:default="{ isActive }">
             <v-card title="Options">
                 <v-card-text>
+                    <span v-show="!isConnected">NOT CONNECTED TO SERVER</span>
                     <SharedOptions :is-dialog-opened="isDialogOpened" :client-config="clientConfig"
                         @config-changed="(newConf) => emit('configChanged', newConf)" />
                     Visible graphics
